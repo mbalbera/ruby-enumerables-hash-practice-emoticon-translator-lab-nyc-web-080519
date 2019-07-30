4157803989103d7902fa388ed6ec3c8077ea2f47
Mason Balbera <mhbalbera@gmail.com>
2019-07-30 15:09:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   end
 end
 
-end
+
 
 def get_english_meaning
  (file_path = './lib/emoticons.yml', japanese_emoticon)
