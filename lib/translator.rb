82799457238f3cab83406942d2e35a7a18147d68
Mason Balbera <mhbalbera@gmail.com>
2019-07-30 15:28:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(path)
     emo_hash["get_emoticon"] = Hash.new
   emo_hash["get_meaning"] = Hash.new
 
-  emoticons.each do |english_word, emoticon_set|
+  emo.each do |english_word, emoticon_set|
     emo_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
     emo_hash["get_meaning"][emoticon_set.last] = english_word
   end
