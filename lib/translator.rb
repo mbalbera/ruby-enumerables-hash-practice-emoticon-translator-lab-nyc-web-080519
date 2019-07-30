716abd4090acade88976b3ef76d810353f6c41ae
Mason Balbera <mhbalbera@gmail.com>
2019-07-30 15:16:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,14 @@ require "yaml"
 def load_library(path)
   emo = YAML.load_file(path)
   emo_hash = {}
-  
+    emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
+
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  end
+  emoticon_hash
 end
 
 def get_japanese_emoticon
